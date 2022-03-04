c402fdf85d6d64a1b4c1346bc403014a11b842eb
Vladimir3456787 <100952199+Vladimir3456787@users.noreply.github.com>
2022-03-04 22:29:59 +0700

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,21 @@
+#подключение библиотеки телеграм
+import telebot
+a = telebot.TeleBot("5136364602:AAEuUnIiB7-hYtwuwJ3kRfiDS8jLUBW7MYo")
+keyboard_test = telebot.types.ReplyKeyboardMarkup(True)
+keyboard_test.row("Привет", "Пока", "Удалить клавиатуру")
+@a.message_handler(commands=['start'])
+def startWork(message):
+ tid = message.chat.id
+ a.send_message(tid, "start work!",reply_markup = keyboard_test)
+@a.message_handler(content_types=['text'])
+def sendYourMessage(message):
+ mid = message.chat.id
+ if message.text == "Привет":
+   a.send_message(mid, "Привет")
+ elif message.text == "Удалить клавиатуру":
+   a.send_message(mid, "Клавиатура удалена", reply_markup=telebot.types.ReplyKeyboardRemove())
+ elif message.text =="Пока":
+   a.send_message(mid, "Пока")
+ else:
+   a.send_message(mid, "Не понимаю тебя")
+a.polling()
